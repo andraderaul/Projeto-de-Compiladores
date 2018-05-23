bdf6eac1785470851a686e3457c27ba7f0f0fff1
andraderaul <theandraderaul@gmail.com>
2018-05-22 22:06:27 -0300

    first commit

---
+++ src/compiladores/Main.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compiladores;
+
+import compiladores.lexer.*;
+import java.io.FileReader;
+import java.io.PushbackReader;
+import compiladores.node.* ;
+
+/**
+ *
+ * @author howl
+ */
+
+public class Main {
+   public static void main(String[] args) {
+      if (args.length > 0) {
+         try {
+            /* Form our AST */
+            Lexer lexer = new Lexer (new PushbackReader(
+               new FileReader(args[0]), 1024));
+           
+            while (true) {
+                Token token = lexer.next();
+                
+                if ("".equals(token.getText())){
+                    System.out.println("break");
+                    break;
+                }
+                
+                System.out.println("Texto: " + token.getText() + " " + "| Classe: " + token.getClass().getSimpleName());
+                    
+           }
+         }
+         catch (Exception e) {
+            System.out.println (e) ;
+         }
+      } else {
+         System.err.println("usage: java simpleAdder inputFile");
+         System.exit(1);
+      }
+   }
+}
+
